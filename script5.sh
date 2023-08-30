ee92d554e719d01ea461682995b66363a6ce1fbc
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2023-08-30 17:48:06 +0100

    While and For Loops

--- script5.sh
+++ script5.sh
@@ -8,3 +8,18 @@ while [ $COUNTER -lt 10 ]; do
 	# let COUNTER=COUNTER+1
 	# let COUNTER+=1
 done
+
+
+for myfile in ls *.txt ; do
+	cat $myfile
+done
+
+
+for x in {1..10}; do
+	echo "X = $x"
+done
+
+
+for (( i=1; i<=10; i++ )); do
+	echo "Number I = $i"
+done
