d6c1e160e18f8b2be1e11c67cce32ba747642b6c
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2023-09-12 12:33:56 +0100

    Fixed

---
+++ script8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for currentNumber in {1..10}
+do
+	echo $currentNumber
+	sleep 0.5
+done
+
+echo "This is outside of the loop."
--- update.sh
+++ update.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-releaseFile=/etc/os-release
+releaseFile = /etc/os-release
+logFile = /var/log/update.log
+logUpdateError= /var/log/update_errors.log
 
 if grep -q "Arch" $releaseFile
 # ArchLinux update
 then
-	sudo pacman -Syu
+	sudo pacman -Syu 1>>$logFile 2>>$logUpdateError
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check the $logUpdateError file."
+	fi
 fi
 
 if grep -q "Ubuntu" $releaseFile || grep -q "Ubuntu" $releaseFile
 # Debian or Ubuntu update
 then
-	sudo apt update
-	sudo apt dist-upgrade
+	sudo apt update 1>>$logFile 2>>$logUpdateError
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check the $logUpdateError file."
+	fi
+	sudo apt dist-upgrade 1>>$logFile 2>>$logUpdateError
+	if [ $? -ne 0 ]
+        then
+		echo "An error occured, please check the $logUpdateErroe file."
+	fi
 fi
