786b91aa04b8f128c3ce3f62431a773869258575
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2023-09-13 16:09:40 +0100

    Function added

--- update.sh
+++ update.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+
+
 releaseFile = /etc/os-release
 logFile = /var/log/update.log
 logUpdateError= /var/log/update_errors.log
+
+
+checkExitStatus() {
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, plese check the $logUpdateError file."
+	fi
+}
 
 if grep -q "Arch" $releaseFile
 # ArchLinux update
 then
 	sudo pacman -Syu 1>>$logFile 2>>$logUpdateError
-	if [ $? -ne 0 ]
-	then
-		echo "An error occured, please check the $logUpdateError file."
-	fi
+	checkExitStatus
 fi
 
 if grep -q "Ubuntu" $releaseFile || grep -q "Ubuntu" $releaseFile
 # Debian or Ubuntu update
 then
 	sudo apt update 1>>$logFile 2>>$logUpdateError
-	if [ $? -ne 0 ]
-	then
-		echo "An error occured, please check the $logUpdateError file."
-	fi
+	checkExitStatus
+
 	sudo apt dist-upgrade 1>>$logFile 2>>$logUpdateError
-	if [ $? -ne 0 ]
-        then
-		echo "An error occured, please check the $logUpdateErroe file."
-	fi
+	checkExitStatus
 fi
